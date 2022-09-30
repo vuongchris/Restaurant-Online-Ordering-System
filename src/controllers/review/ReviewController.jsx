222a1071c4a4d646dc9f67f981d1c07dc8c4db99
Chris Vuong <98904113+vuongchris@users.noreply.github.com>
2022-09-30 21:44:00 +1000

    Review Page Update

--- src/controllers/review/ReviewController.jsx
+++ src/controllers/review/ReviewController.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { collection, addDoc } from 'firebase/firestore';
 import ReviewView from '../../views/review/ReviewView';
 import ReviewHistoryView from '../../views/review/ReviewHistoryView';
+import { db } from '../../firebase';
 
 function ReviewController({ view }) {
+  const ratingRef = useRef();
+  const descriptionRef = useRef();
+  const handleReviewSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await addDoc(collection(db, 'review'), {
+        rating: ratingRef,
+        description: descriptionRef,
+      });
+    } catch (ex) {
+      // eslint-disable-next-line no-console
+      console.log(ex);
+    }
+  };
+
   const reviewViews = {
-    review: <ReviewView />,
+    review: <ReviewView
+      descriptionRef={descriptionRef}
+      ratingRef={ratingRef}
+      submitReview={handleReviewSubmit}
+    />,
     reviewHistory: <ReviewHistoryView />,
   };
   return reviewViews[view];
--- src/views/review/ReviewView.jsx
+++ src/views/review/ReviewView.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
@@ -5,50 +6,52 @@ import TextField from '@mui/material/TextField';
 import Rating from '@mui/material/Rating';
 import Button from '@mui/material/Button';
 
-function ReviewView() {
+function ReviewView({ descriptionRef, ratingRef, handleReviewSubmit }) {
   const [value, setValue] = React.useState(2);
   return (
     <div>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-      >
-        <Grid item>
-          <Typography variant="h3">Review</Typography>
+      <form onSubmit={handleReviewSubmit}>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          <Grid item>
+            <Typography variant="h3">Review</Typography>
+          </Grid>
+          <Grid item>
+            <Rating
+              defaultValue={null}
+              value={value}
+              precision={0.5}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+              inputRef={ratingRef}
+            />
+            <TextField
+              style={{ width: '100%', fontSize: '20px' }}
+              label="Review Description"
+              required
+              multiline
+              rows={5}
+              maxRows={20}
+              inputRef={descriptionRef}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+            >
+              Submit
+            </Button>
+          </Grid>
         </Grid>
-
-        <Grid item>
-          <Rating
-            name="review-rating"
-            defaultValue={null}
-            value={value}
-            precision={0.5}
-            onChange={(event, newValue) => {
-              setValue(newValue);
-            }}
-          />
-          <TextField
-            style={{ width: '100%', fontSize: '20px' }}
-            label="Review Description"
-            required
-            multiline
-            rows={5}
-            maxRows={10}
-          />
-        </Grid>
-        <Grid>
-          <Button
-            type="submit"
-            variant="contained"
-            size="large"
-          >
-            Submit
-          </Button>
-        </Grid>
-      </Grid>
+      </form>
     </div>
   );
 }
