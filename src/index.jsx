ef58d72055946a2c6b822f0805cacbfbb6626528
moostofa <mmdrahinz96@gmail.com>
2022-10-05 05:41:26 +1100

    fixed homesreen not showing

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
