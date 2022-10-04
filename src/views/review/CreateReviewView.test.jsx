f7c86e32fa689e2a739cf618ea6fafcd51a95588
Chris Vuong <98904113+vuongchris@users.noreply.github.com>
2022-10-05 09:49:53 +1100

    removed eslint errors

--- src/views/review/CreateReviewView.test.jsx
+++ src/views/review/CreateReviewView.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { render, screen, getDefaultNormalizer } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
