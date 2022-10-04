edec2fdafe5526e5aafa0a051b6e270ba98da734
Chris Vuong <98904113+vuongchris@users.noreply.github.com>
2022-10-04 21:38:56 +1100

    Updated tests to allow useNavigate()

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </BrowserRouter>,
+  document.getElementById('root'),
 );
