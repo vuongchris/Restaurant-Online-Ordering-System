b0a001869be62c94e8a5c69f7760edc094d6f9bf
MatthewHKC <KevinMatthew.Chow-1@student.uts.edu.au>
2022-10-04 21:02:11 +0700

    Update and Delete function added

--- src/controllers/infoDisplay/InfoDisplayController.jsx
+++ src/controllers/infoDisplay/InfoDisplayController.jsx
@@ -1,13 +1,50 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { collection, getDocs } from 'firebase/firestore';
+import {
+  collection, getDocs, addDoc, deleteDoc, setDoc, query, where, updateDoc, doc,
+} from 'firebase/firestore';
 import React, { useState, useEffect } from 'react';
 import InfoDisplayView from '../../views/infoDisplay/InfoDisplayView';
 import { db } from '../../firebase';
 
 function InfoDisplayController({ view }) {
+  // Add the Create function into the controller
+  const [newRestaurant, setNewRestaurant] = useState('');
+  const [newLocation, setNewLocation] = useState('');
+  const [newDistance, setNewDistance] = useState('');
+
   const [infoDisplay, setInfoDisplays] = useState([]);
   const infoDisplayCollection = collection(db, 'restaurant');
+
+  const addRestaurant = async () => {
+    await addDoc(infoDisplayCollection, {
+      restaurantBranch: newRestaurant,
+      Location: newLocation,
+      Distance: newDistance,
+    });
+  };
+
+  const updateRestaurant = async () => {
+    const q = query(collection(db, 'restaurant'), where('restaurantBranch', '==', newRestaurant));
+    // basically gets a query going for specifically restaurant docs
+    const querySnapshot = await getDocs(q);
+    let id = '';
+    querySnapshot.forEach(async (doc) => {
+    // this line will get the document id of the restaurant
+      id = doc.id;
+      const getRestaurant = doc(db, 'restaurant', id);
+      // Set the updated document
+      await updateDoc(getRestaurant, {
+        restaurantBranch: newRestaurant,
+        location: newLocation,
+      });
+    });
+  };
+
+  const deleteBranch = async (id) => {
+    const restaurantDoc = doc(db, 'users', id);
+    await deleteDoc(restaurantDoc);
+  };
 
   useEffect(() => {
     const getInfoDisplay = async () => {
@@ -41,7 +78,16 @@ function InfoDisplayController({ view }) {
   // };
 
   // return testViews[view];
-  return <InfoDisplayView infoDisplay={infoDisplay} />;
+  return (
+    <InfoDisplayView
+      infoDisplay={infoDisplay}
+      addRestaurant={addRestaurant}
+      setNewRestaurant={setNewRestaurant}
+      setNewLocation={setNewLocation}
+      setNewDistance={setNewDistance}
+      deleteBranch={deleteBranch}
+    />
+  );
 }
 
 export default InfoDisplayController;
--- src/views/infoDisplay/InfoDisplayView.jsx
+++ src/views/infoDisplay/InfoDisplayView.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import './table.css';
+// import createRestaurant from '../../controllers/infoDisplay/InfoDisplayController';
 // import '../../controllers/infoDisplay/InfoDisplayController';
 
-function InfoDisplayView({ infoDisplay }) {
+function InfoDisplayView({
+  infoDisplay, addRestaurant, setNewRestaurant, setNewLocation, setNewDistance, deleteBranch,
+}) {
   return (
-    <div>
-      {infoDisplay.map((restaurant) => (
+    <>
+      <div>
+
         <table>
           <tr>
             <th>Restaurant Name</th>
@@ -14,17 +18,30 @@ function InfoDisplayView({ infoDisplay }) {
             <th>Distance</th>
             <th>Operating Hours</th>
             <th>Allow Pickups?</th>
+            <th>Delete Selected Branch</th>
           </tr>
-          <tr>
-            <td>{restaurant.restaurantBranch}</td>
-            <td>{restaurant.Location}</td>
-            <td>{restaurant.Distance}</td>
-            <td>{restaurant.Hours}</td>
-            <td>{restaurant.pickUp}</td>
-          </tr>
+
+          {infoDisplay.map((restaurant) => (
+
+            <tr>
+              <td>{restaurant.restaurantBranch}</td>
+              <td>{restaurant.Location}</td>
+              <td>{restaurant.Distance}</td>
+              <td>{restaurant.Hours}</td>
+              <td>{restaurant.pickUp}</td>
+              <button type="submit" onClick={() => { deleteBranch(); }}>Delete Branch</button>
+            </tr>
+          ))}
         </table>
-      ))}
-    </div>
+
+      </div>
+      <input placeholder="branch name..." onChange={(event) => { setNewRestaurant(event.target.value); }} />
+      <input placeholder="branch location..." onChange={(event) => { setNewLocation(event.target.value); }} />
+      <input placeholder="distance..." onChange={(event) => { setNewDistance(event.target.value); }} />
+      <button type="submit" onClick={addRestaurant}> test button </button>
+      <div />
+
+    </>
   );
 }
 
