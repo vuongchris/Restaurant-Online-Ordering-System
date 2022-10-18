702eacd5b03d09def18900ea86ab2ddce04015ab
dylan9108 <71366783+dylan9108@users.noreply.github.com>
2022-10-18 14:40:14 +0800

    update customer service

--- src/views/customer_service/CustomerService.jsx
+++ src/views/customer_service/CustomerService.jsx
@@ -1,54 +1,65 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable react/prop-types */
-import { Button, TextField } from '@mui/material';
-import React from 'react';
-import { useNavigate } from 'react-router';
+// eslint-disable-next-line no-unused-vars
+import { Button, Grid, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../../firebase';
 
-function CustomerService({ newCustomerTicket, handleSubmit }) {
-  // Redirect the user to another page using this
-  const navigate = useNavigate();
+function CustomerService() {
+  const [newCustomerTicket, setCustomerTicket] = useState('');
+
+  const customerServiceRef = collection(db, 'customerTicket');
+
+  const handleSubmitButton = async () => {
+    try {
+      await addDoc(customerServiceRef, {
+        customerTicket: newCustomerTicket,
+      });
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error('Error, cant create ticket: ', e);
+    }
+  };
+
   return (
     <div
       style={{ textAlign: 'center' }}
     >
+      {/* might change to some other user story, too hard to implement */}
       <h1>Welcome to the Customer Service</h1>
       <div>
         Please state what assistant do you need and our agent will get back to you shortly.
       </div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <TextField
-            style={{
-              width: '30%',
-              fontSize: '20px',
-              margin: '20px',
-            }}
-            label="Customer Service"
-            multiline
-            minRows={5}
-            maxRows={10}
-            required
-            inputRef={newCustomerTicket}
-          />
-          <br />
-          <Button
-            type="submit"
-            variant="contained"
-            size="large"
-          >
-            Submit
-          </Button>
-          <br />
-          <br />
-          <Button
-            variant="contained"
-            size="large"
-            color="error"
-            onClick={() => navigate('/')}
-          >
-            Cancel
-          </Button>
-        </div>
-      </form>
+      <div>
+        <TextField
+          style={{ width: '30%', fontSize: '20px', margin: '20px' }}
+          label="Customer Service"
+          multiline
+          minRows={5}
+          maxRows={10}
+          required
+          onChange={(event) => {
+            setCustomerTicket(event.target.value);
+          }}
+        />
+        <br />
+        <Button
+          variant="contained"
+          size="large"
+          onClick={handleSubmitButton}
+        >
+          Submit
+        </Button>
+        <br />
+        <br />
+        <Button
+          variant="contained"
+          size="large"
+        >
+          Cancel
+        </Button>
+      </div>
     </div>
 
   );
