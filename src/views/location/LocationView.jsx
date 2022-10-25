04f87388b5ab88c228b6f238c341f7e34e3cfa2f
3-seconds-slow <brendanoates@gmail.com>
2022-10-26 03:55:33 +1100

    Location Service Functional commit

--- src/views/location/LocationView.jsx
+++ src/views/location/LocationView.jsx
@@ -1,35 +1,75 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import {
+  GoogleMap, LoadScript, MarkerF, InfoWindowF,
+} from '@react-google-maps/api';
 
-function LocationView({ locations }) {
-  // locationList.forEach((doc) => { <li>{doc.data()}</li>; });
-  // const list = locationList.map((d) => (<li key={d.name}> {d.data} </li>));
+function LocationView({
+  locations, containerStyle, activeMarker, setActiveMarker, handleActiveMarker, center, onLoad,
+}) {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Locations</h1>
-      {locations.map((location, index) => (
-        <div key={location.Name}>
-          <h2>
-            Location #
-            {index + 1}
-          </h2>
-          <h4>
-            Name:
-            {' '}
-            {location.Name}
-          </h4>
-          <h4>
-            Address:
-            {' '}
-            {location.Address}
-          </h4>
-          <h4>
-            City:
-            {' '}
-            {location.City}
-          </h4>
-        </div>
-      ))}
+    <div>
+      <div>
+        <h1>Map</h1>
+        <LoadScript
+          googleMapsApiKey="AIzaSyDVbjAei-dnyBW3YowW5l2KsDZXsrlCgn4"
+        >
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={15}
+            onClick={() => setActiveMarker(null)}
+          >
+            {locations.map(({
+              id, Name, Address, Lat, Lng,
+            }) => (
+              <MarkerF
+                onLoad={onLoad}
+                key={id}
+                position={{ lat: Lat, lng: Lng }}
+                onClick={() => handleActiveMarker(id, Name)}
+              >
+                {activeMarker === id ? (
+                  <InfoWindowF onCloseClick={() => setActiveMarker(null)}>
+                    <div>
+                      {Name}
+                      {Address}
+                    </div>
+                  </InfoWindowF>
+                ) : null}
+              </MarkerF>
+            ))}
+          </GoogleMap>
+        </LoadScript>
+      </div>
+      <div>
+        <h1>Locations</h1>
+        {locations.map((location, index) => (
+          <div key={location.Name}>
+            <h2>
+              Location #
+              {index + 1}
+            </h2>
+            <h4>
+              Name:
+              {' '}
+              {location.Name}
+            </h4>
+            <h4>
+              Address:
+              {' '}
+              {location.Address}
+            </h4>
+            <h4>
+              City:
+              {' '}
+              {location.City}
+            </h4>
+            <button onClick={() => handleActiveMarker(location.id, location.Name)}>Show</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
